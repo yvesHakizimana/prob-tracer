9fa60cf7281b5bab5746f409ec29fe8800d4815f
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-24 15:08:07 +0200

    Building delete issue page and functionality.

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -3,42 +3,65 @@ import {Button, Flex, AlertDialog} from "@radix-ui/themes";
 import {Cross1Icon} from "@radix-ui/react-icons";
 import axios from "axios";
 import {useRouter} from "next/navigation";
+import {useState} from "react";
+
+
 
 const DeleteIssueButton = ({issueId} : {issueId: number}) => {
     const router = useRouter()
-    return (
+    const [error, setError] = useState(false);
 
-        <AlertDialog.Root>
-            <AlertDialog.Trigger>
-                <Button color='red'>
-                    <Cross1Icon />
-                    Delete Issue
-                </Button>
-            </AlertDialog.Trigger>
-            <AlertDialog.Content maxWidth="450px">
-                <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-                <AlertDialog.Description size="2">
-                    Are you sure you want to delete this issue? This action cannot be undone.
-                </AlertDialog.Description>
-                <Flex gap="3" mt="4" justify="end">
+    const deleteIssue = async () => {
+        try {
+            await axios.delete('/api/issuess/' + issueId);
+            router.push('/issues')
+            router.refresh();
+        } catch (error) {
+            setError(true);
+        }
+    }
+    return (
+        <>
+            <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <Button color='red'>
+                        <Cross1Icon />
+                        Delete Issue
+                    </Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content maxWidth="450px">
+                    <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+                    <AlertDialog.Description size="2">
+                        Are you sure you want to delete this issue? This action cannot be undone.
+                    </AlertDialog.Description>
+                    <Flex gap="3" mt="4" justify="end">
+                        <AlertDialog.Cancel>
+                            <Button variant="soft" color="gray">
+                                Cancel
+                            </Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                            <Button variant="solid" color="red" onClick={deleteIssue}>
+                                Delete this issue
+                            </Button>
+                        </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+            <AlertDialog.Root open={error}>
+                <AlertDialog.Content>
+                    <AlertDialog.Title>Error</AlertDialog.Title>
+                    <AlertDialog.Description size="2">
+                        This issue could not be deleted.
+                    </AlertDialog.Description>
                     <AlertDialog.Cancel>
-                        <Button variant="soft" color="gray">
-                            Cancel
+                        <Button variant="soft" color="gray" mt='4' onClick={ () => {setError(false);} }>
+                            OK
                         </Button>
                     </AlertDialog.Cancel>
-                    <AlertDialog.Action>
-                        <Button variant="solid" color="red" onClick={ async () => {
-                            await axios.delete('/api/issues/' + issueId);
-                            router.push('/issues')
-                            router.refresh();
-                        } }>
-                            Delete this Issue
-                        </Button>
-                    </AlertDialog.Action>
-                </Flex>
-            </AlertDialog.Content>
-        </AlertDialog.Root>
-
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+        </>
     )
 }
 
