64494f6ba8464d309e77dff449d75bb0bca69a83
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-25 07:45:07 +0200

    Test of the login and logout pages.

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -10,7 +10,8 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
         })
-    ]
+    ],
+    session: { strategy: 'jwt'}
 })
 
 export { handler as GET, handler as POST }
