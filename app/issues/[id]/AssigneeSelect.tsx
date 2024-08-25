15d6ce1d527e9c203777ee456d4f82bc4993c795
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-25 14:02:23 +0200

    Code refactoring in assignee components.

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -7,32 +7,29 @@ import {Skeleton} from "@/app/components";
 import toast , {Toaster}  from "react-hot-toast";
 
 const AssigneeSelect = ({issue} : { issue: Issue}) => {
-    const {data: users, error, isLoading} = useQuery<User[]>({
-        queryKey: ['users'],
-        queryFn: () => axios.get('/api/users').then(res => res.data),
-        staleTime: 60 * 1000,
-        retry: 7
-    })
+    const {data: users, error, isLoading} = useUsers();
 
     if(error) return null;
 
     if(isLoading) return <Skeleton />
 
+    const assignIssue = (userId: string) => {
+        console.log(userId)
+        axios
+            .patch('/api/issues/' + issue.id, { assignedToUserId : userId || null })
+            .catch(() => toast.error("Changes could not be made."))
+    }
+
     return (
         <>
             <Select.Root
-                defaultValue={issue.assignedToUserId || " "}
-                onValueChange={(userId) => {
-                    console.log(userId)
-                    axios
-                        .patch('/api/issues/' + issue.id, { assignedToUserId : userId || null })
-                        .catch((err) => toast.error("Changes could not be made."))
-                }}>
-                <Select.Trigger placeholder='Assign.....'/>
+                defaultValue={issue.assignedToUserId || "."}
+                onValueChange={assignIssue}>
+                <Select.Trigger placeholder='Assign....'/>
                 <Select.Content>
                     <Select.Group>
                         <Select.Label>Suggestions</Select.Label>
-                        <Select.Item value=" ">Unassigned</Select.Item>
+                        <Select.Item value=".">Unassigned</Select.Item>
                         {users?.map(user =>
                             <Select.Item
                                 key={user.id}
@@ -52,8 +49,14 @@ const AssigneeSelect = ({issue} : { issue: Issue}) => {
             </Select.Root>
             <Toaster />
         </>
-
     )
 }
+
+const useUsers = () => useQuery<User[]>({
+    queryKey: ['users'],
+    queryFn: () => axios.get('/api/users').then(res => res.data),
+    staleTime: 60 * 1000,
+    retry: 7
+})
 
 export default AssigneeSelect
