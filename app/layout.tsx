778d7d2d89e412fecbd98163b3d952c538d65729
Yves HAKIZIMANA <yvhakizimana123@gmail.com>
2024-08-27 10:43:31 +0200

    Removing unnecessary fonts.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import '@radix-ui/themes/styles.css';
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import NavBar from "@/app/NavBar";
 import {Container, Theme
 } from '@radix-ui/themes';
